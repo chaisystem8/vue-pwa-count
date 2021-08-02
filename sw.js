a76abf09304ededed26367fc28cc5d919337ff54
Isaias <isaias.soto.le@gmail.com>
2021-08-02 16:52:02 -0500

    PWA Service Worker rutas cache

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "V1_cache_contador_app_vue"
+const urlsToCache = [
+    "./",
+    "./img/favicon.png",
+    "./img/32.png",
+    "./img/64.png",
+    "./img/128.png",
+    "./img/256.png",
+    "./img/512.png",
+    "./img/1024.png",
+    "./js/main.js",
+    "https://unpkg.com/vue@next",
+    "./js/mountApp.js",
+    "./css/style.css",
+    "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+];
