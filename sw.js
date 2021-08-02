c87be484e18b88aae9965bb2fefb53af1d0df12e
Isaias <isaias.soto.le@gmail.com>
2021-08-02 17:17:09 -0500

    PWA Service Worker finish

--- sw.js
+++ sw.js
@@ -14,3 +14,51 @@ const urlsToCache = [
     "./css/style.css",
     "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
 ];
+
+self.addEventListener("install", e =>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(
+            cache => cache.addAll(urlsToCache).then(
+                () => self.skipWaiting()
+            ).catch(
+                err => console.log(err)
+            )
+        )
+    )
+})
+
+self.addEventListener("activate", e => {
+    const cacheWhiteList = [CACHE_NAME]
+
+    e.waitUntil(
+        caches.keys().then(
+            cacheNames => {
+                return Promise.all(
+                    cacheNames.map(
+                        cacheName => {
+                            if(cacheWhiteList.indexOf(cacheName) === -1){
+                                return caches.delete(cacheName)
+                            }
+                        }
+                    )
+                )
+            }
+        ).then(
+            () => self.clients.claim()
+        )
+    )
+})
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(
+            res => {
+                if(res){
+                    return res
+                }
+                
+                return fetch(e.request)
+            }
+        )
+    )
+})
